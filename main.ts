0ce6b7b3dbea02fa2cce03dd3b3140d59ff15dba
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2023-03-29 23:12:32 +0200

    initial commit.

---
+++ main.ts
@@ -0,0 +1,34 @@
+import { load } from "std/dotenv/mod.ts";
+import { cron } from "cron";
+import { Telegraf } from "telegraf";
+
+const env = await load({
+  restrictEnvAccessTo: ["TELEGRAM_TOKEN", "GROUP_ID", "CRON_SCHEDULE"],
+});
+
+const bot = new Telegraf(env.TELEGRAM_TOKEN);
+
+bot.command("mensa", async (ctx) => {
+  await ctx.sendPoll(
+    "Heute Mensa?",
+    ["11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "später"],
+    { is_anonymous: false }
+  );
+  console.log("Sending Mensa Poll to Chat: ", await ctx.getChat());
+});
+
+if (env.CRON_SCHEDULE !== undefined) {
+  cron(env.CRON_SCHEDULE, () => {
+    if (env.GROUP_ID !== undefined) {
+      console.log("Sending Mensa Poll to default ID: ", env.GROUP_ID);
+      bot.telegram.sendPoll(
+        env.GROUP_ID,
+        "Heute Mensa?",
+        ["11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "später"],
+        { is_anonymous: false }
+      );
+    }
+  });
+}
+
+bot.launch();
