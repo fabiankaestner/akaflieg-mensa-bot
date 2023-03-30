0a97c71f2885e155fdf0ee70335efef4139682dd
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2023-03-30 14:29:01 +0200

    Test environment access

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import { Telegraf } from "telegraf";
 const env = await load({
   restrictEnvAccessTo: ["TELEGRAM_TOKEN", "GROUP_ID", "CRON_SCHEDULE"],
 });
+console.log(Deno.env.get("TELEGRAM_TOKEN"));
+console.log(env);
 
 const bot = new Telegraf(env.TELEGRAM_TOKEN);
 
